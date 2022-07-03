ae083d028eaad6e2b5850c533527c1ee807e0e03
hagverdi11 <hagverdiam@code.edu.az>
2022-07-04 01:01:42 +0400

    Added new Homework table

---
+++ Power/Static- Extension/Extensions/Mathematic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Static__Extension.Extensions
+{
+    public static class Mathematic
+    {
+       public static double Power(this double number1, double number2)
+       {
+            return Math.Pow(number1, number2);
+
+
+       }
+    }
+}
---
+++ Power/Static- Extension/Program.cs
@@ -0,0 +1,16 @@
+﻿using Static__Extension.Extensions;
+using System;
+
+namespace Static__Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1 = 5;
+            var result = number1.Power(3);
+            Console.WriteLine(result);
+
+        }
+    }
+}
